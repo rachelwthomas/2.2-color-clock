86938c6fecf624cb0fc7117817d4b42bb8004ed1
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-15 15:32:26 -0500

    clock styled

---
+++ main.js
@@ -0,0 +1,86 @@
+// immediately invoked function expression-prevent you from overiding global variables
+
+// google how to convert hexcode into time, base 16
+
+// event listener
+
+
+(function() {
+  'use strict';
+
+  // let $container = document.querySelector('.container');
+  // let $element = document.querySelector('span');
+
+  // console.log($element);
+
+  // $element.textContent = "Hello, Sally!";
+  //
+  //
+  // let $variable = document.querySelector('#purple');
+  //
+  // console.log($variable.textContent);
+
+
+// this is the same thing as what is abvoe
+  // console.log(document.querySelector('#purple').textContent);
+
+// new Date()
+
+// set setTimeout
+
+// set interval
+
+
+// let currentTime;
+// console.log(date);
+
+// setInterval(function, ms);
+  // setInterval(() => {
+  //   currentTime = new Date();
+  //   console.log(currentTime);
+  // },1000);
+
+//   let getCurrentTime = () => {
+//     currentTime = new Date ();
+//     console.log(currentTime);
+//   }
+//
+//  setInterval(getCurrentTime, 1000);
+//
+//  let $button = document.querySelector('button');
+//
+//  $button.addEventListener('click', () => {
+//  console.log('you clicked a button');
+// });
+
+let $clock = document.querySelector('time');
+document.write(Date($clock));
+console.log($clock)
+
+document.querySelector("p1").innerHTML = "New text!";
+
+// let currentTime = () => {
+//   currentTime = new Date ();
+//   console.log(currentTime);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
